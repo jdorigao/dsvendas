13d77c08fbb9f3242c81faaaffed67e812b22842
Juliano Dorigão <jdorigao@gmail.com>
2021-05-03 12:02:14 -0400

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/jdorigao" target="_blank" rel="noreferrer">Juliano Dorigão</a></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
