763c7bdfece9f52ec20f81ccedf5b54935c329e5
Juliano Dorigão <jdorigao@gmail.com>
2021-05-05 16:20:37 -0400

    Pagination

---
+++ backend/src/main/java/com/br/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,7 @@
+package com.br.dsvendas.repositories;
+
+import com.br.dsvendas.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+}
